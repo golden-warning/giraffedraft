9eaa3b7fa7725fecec6bd9311c3959cacdecbf3c
pouncealot <spamtoheck@gmail.com>
2014-11-07 16:14:54 -0800

    new stuff

--- chrome/content.js
+++ chrome/content.js
@@ -1,1 +1,17 @@
-console.log('content.js');
+angular.module('gDraft',[])
+
+.controller('gDController', function($scope, $http){
+	$scope.person = {
+		name: "Warren"
+	};
+	$scope.calculate = function(){
+		$http.get('http://giraffedraft.azurewebsites.net/api/init').
+		success(function(data, status, headers, config){
+			console.log(data)
+			$scope.person.name = data[0].NAME;
+		}).
+		error(function(data, status, headers, config){
+			console.log('failed!!!!!!!!!!!')
+		})
+	}
+});
