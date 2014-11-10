0560884a05433b921ff5175c5121cd3d3ede17db
pouncealot <spamtoheck@gmail.com>
2014-11-10 12:19:26 -0800

    begin restructure

---
+++ chrome/directives/directives.js
@@ -0,0 +1,22 @@
+;(function(){
+  'use strict';
+  /**
+   * Removes server error when user updates input
+   */
+  angular
+    .module('gDraft')
+    .directive('gDraft.directives', calculate);
+
+  /* @inject */
+  function mongooseError() {
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+          element.on('keydown', function() {
+            return ngModel.$setValidity('mongoose', true);
+          });
+        }
+      };
+  }
+}).call(this);
