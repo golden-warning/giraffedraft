07e85ce2ceb8e727725b9abb660b74a14308c84d
pouncealot <spamtoheck@gmail.com>
2014-11-10 15:04:20 -0800

    begin changes after rebase

--- chrome/services/services.js
+++ chrome/services/services.js
@@ -1,1 +1,15 @@
-services.js
+angular.module('gDraft', ['pageslide-directive', 'gDraft.services'])
+  
+  $http.get('http://giraffedraft.azurewebsites.net/api/init').
+  success(function(data, status, headers, config){
+    $scope.undrafted = data;
+    $scope.allPlayers = angular.copy(data);
+    // for(var i = 0; i < undrafted.length; i++){
+    //   undrafted[i].drafted = false;
+    // }
+    $scope.calculate();
+
+  }).
+  error(function(data, status, headers, config){
+    console.log('failed to get initial players')
+  })
